5c29bc0341734e5f4691ac744cbcedc6bace1e62
minseo121 <kittse1217@gmail.com>
2023-05-23 20:52:26 +0900

    공부중

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TodoList(models.Model):
+    todos = models.CharField(max_length=30)
+    description = models.TextField()
+    important = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f'{self.todos} [{self.id}]'
